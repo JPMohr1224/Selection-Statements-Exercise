cf8717ab7e314586b4254067ab6a22403dd43031
JP Mohr <James.Mohr2.0@hotmail.com>
2024-04-30 14:39:08 -0400

    Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            // The following code will allow the user to guess what your favorite number is
+            // (be sure to prompt the user what to input):
+            Console.WriteLine("Try to guess my favorite number?");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too Low!");
+            } else if(userInput > favNumber)
+            {
+                Console.WriteLine("Too high.");
+            } else
+            {
+                Console.WriteLine("You got it!");
+            }
         }
     }
 }
